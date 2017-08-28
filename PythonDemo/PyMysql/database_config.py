17783ed418c599a5ed9f191fbeabefb49f0d3774
LeeKai <bylikai@163.com>
2017-08-29 07:26:36 +0800

    add the configure of mysql

---
+++ PythonDemo/PyMysql/database_config.py
@@ -0,0 +1,13 @@
+"""
+This is the configure for mysql database.
+"""
+
+def func_database_config():
+    """
+    return the database configure
+    """
+    host = "127.0.0.1"
+    username = "root"
+    password = "root"
+    dbname = "dbbase"
+    return host, username, password, dbname
